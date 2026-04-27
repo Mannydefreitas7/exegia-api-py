d39dc21de7380b25868a00f8b211b5370fde5b2d
emmanuel <manny.defreitas7@me.com>
2026-04-26 23:29:57 -0700

    ✨ feat: add app configuration and infrastructure, update Python version, and enhance Docker setup

---
+++ scripts/__init__.py
@@ -0,0 +1,19 @@
+import clean
+import dev_gui
+import generate_ssl
+import publish
+import setup
+import start
+import stop
+import work
+
+__all__ = [
+    "clean",
+    "dev_gui",
+    "generate_ssl",
+    "publish",
+    "setup",
+    "start",
+    "stop",
+    "work",
+]
